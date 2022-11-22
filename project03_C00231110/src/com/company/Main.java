c33a65056da590171098e37e67dd0d694d945f8e
daltonthayer1 <104484704+daltonthayer1@users.noreply.github.com>
2022-11-21 19:04:53 -0600

    Added Task 4
    Added program argument handling and edge case detection

--- project03_C00231110/src/com/company/Main.java
+++ project03_C00231110/src/com/company/Main.java
@@ -8,12 +8,191 @@ import java.util.concurrent.Semaphore;
 public class Main {
 
     public static void main(String[] args) {
-        int quantum = 5;
-        //FCFS();
-        //NPSJF();
-        //PSJF();
-        RR(quantum);
+
+        // Begin code changes by Dalton Thayer
+        int algo = 0;
+        int coreNum = 0;
+        int quantum = 0;
+        Random random = new Random();
+
+        try {
+            try {
+                if (args[0].contains("-S")) {
+                    switch (Integer.parseInt(args[1])) {
+                        case 1:
+                            algo = 1;
+                            break;
+                        case 2:
+                            algo = 2;
+                            break;
+                        case 3:
+                            algo = 3;
+                            break;
+                        case 4:
+                            algo = 4;
+                            break;
+
+                    }
+                    if (args[1].contains("2")) {
+                        quantum = Integer.parseInt(args[2]);
+                        if (args[3].contains("-C")) {
+                            switch (Integer.parseInt(args[4])) {
+                                case 1:
+                                    coreNum = 1;
+                                    break;
+                                case 2:
+                                    coreNum = 2;
+                                    break;
+                                case 3:
+                                    coreNum = 3;
+                                    break;
+                                case 4:
+                                    coreNum = 4;
+                                    break;
+                            }
+                        }
+                    } else if (args[2].contains("-C")) {
+                        switch (Integer.parseInt(args[3])) {
+                            case 1:
+                                coreNum = 1;
+                                break;
+                            case 2:
+                                coreNum = 2;
+                                break;
+                            case 3:
+                                coreNum = 3;
+                                break;
+                            case 4:
+                                coreNum = 4;
+                                break;
+                        }
+                    }
+
+                } else if (args[0].contains("-C")) {
+                    switch (Integer.parseInt(args[1])) {
+                        case 1:
+                            coreNum = 1;
+                            break;
+                        case 2:
+                            coreNum = 2;
+                            break;
+                        case 3:
+                            coreNum = 3;
+                            break;
+                        case 4:
+                            coreNum = 4;
+                            break;
+                    }
+                    if (args[2].contains("-S")) {
+                        switch (Integer.parseInt(args[1])) {
+                            case 1:
+                                algo = 1;
+                                break;
+                            case 2:
+                                algo = 2;
+                                break;
+                            case 3:
+                                algo = 3;
+                                break;
+                            case 4:
+                                algo = 4;
+                                break;
+
+                        }
+                        if (args[3].contains("2")) {
+                            quantum = Integer.parseInt(args[4]);
+                        }
+                    }
+                }
+            } catch (NumberFormatException ex) {
+            }
+
+            while (true) {
+                Scanner in = new Scanner(System.in);
+                int loopControl = 0;
+
+                if ((algo > 0 && algo < 5) && (coreNum > 0 && coreNum < 5)) {
+                    loopControl = 1;
+                }
+
+                if (loopControl == 1) {
+                    break;
+                } else {
+                    System.out.println("Invalid Input");
+                }
+                while (true) {
+                    System.out.println("1. First Come, First Serve");
+                    System.out.println("2. Round Robin");
+                    System.out.println("3. Non-Preemptive SJF");
+                    System.out.println("4. Preemptive SJF");
+                    System.out.println("Enter Algorithm you would like to run: ");
+                    try {
+                        algo = Integer.parseInt(in.nextLine());
+                    } catch (NumberFormatException ex) {
+                    }
+                    if (algo == 2) {
+                        while (true) {
+                            System.out.println("Round robin detected. What would you like time quantum to be? (2-10)");
+                            try {
+                                quantum = Integer.parseInt(in.nextLine());
+                            } catch (NumberFormatException ex) {
+                            }
+                            if (quantum < 11 && quantum > 1) {
+                                break;
+                            } else {
+                                System.out.println("Invalid Input.");
+                            }
+                        }
+                    }
+                    if (algo > 0 && algo < 5) {
+                        break;
+                    }
+                }
+                while (true) {
+                    System.out.println("Enter Number of cores you would like to run with: (1-4) ");
+                    try {
+                        coreNum = Integer.parseInt(in.nextLine());
+                    } catch (NumberFormatException ex) {
+                    }
+                    if (coreNum < 5 && coreNum > 0) {
+                        break;
+                    } else {
+                        System.out.println("Invalid Input.");
+                    }
+                }
+            }
+            /*                                                          Testing and Examples
+            System.out.println(Arrays.toString(args));
+            System.out.println(algo);
+            System.out.println(coreNum);
+            System.out.println(quantum);
+
+            MyThread[] threads = new MyThread[random.nextInt(25)];
+            for (int i = 0; i < threads.length; i++){
+                MyThread thread = new MyThread(i, random.nextInt(50), 'semaphore list start', 'semaphore list end');
+                threads[i] = thread;
+            }
+            */
+
+            switch (algo){
+                case 1:
+                    //FCFS();
+                    break;
+                case 2:
+                    RR(quantum);
+                    break;
+                case 3:
+                    //NSJF();
+                    break;
+                case 4:
+                    //PSJF();
+                    break;
+            }
+        }
+        catch(IndexOutOfBoundsException ex){}
+
     }
+    // End code changes by Dalton Thayer
 
     // Begin code changes by Austin Dugas
     public static void NPSJF(){
